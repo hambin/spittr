029972a999a6cd2b37dec944e539431470b5e94c
hanbing <470467217@qq.com>
2016-11-17 18:21:18 +0800

    备份

--- src/main/java/com/spittr/web/SpitterController.java
+++ src/main/java/com/spittr/web/SpitterController.java
@@ -1,8 +1,11 @@
 package com.spittr.web;
 
+import com.spittr.Spitter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @Controller
 @RequestMapping("/spitter")
@@ -12,4 +15,9 @@ public class SpitterController {
 	public String showRegistationForm(){
 		return "registerForm";
 	}
+
+//	@RequestMapping(value="register", method=RequestMethod.POST)
+//	public String processRegistration(
+//		@RequestPart("profilePicture") byte[] profilePicture, @Valid Spitter spitter,Errors errors
+//	){}
 }
